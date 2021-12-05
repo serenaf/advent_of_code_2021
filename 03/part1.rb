0622e28445652aa8e5b10044219c1489e421f3fb
Serena Fritsch <serena@intercom.io>
2021-12-05 18:24:46 +0000

    much better solution, and finally day2

--- 03/part1.rb
+++ 03/part1.rb
@@ -1,92 +1,34 @@
 class Part1
+  attr_reader :input
 
   def initialize
-    @input =  File.readlines("input2.txt", chomp: true)
-    @position1 = []
-    @position2 = []
-    @position3 = []
-    @position4 = []
-    @position5 = []
-    @position6 = []
-    @position7 = []
-    @position8 = []
-    @position9 = []
-    @position10 = []
-    @position11 = []
-    @position12 = []
+    @input = File.readlines("input.txt", chomp: true)
+  end
 
 
-    @input.each do |line|
-      @position1 << line[0]
-      @position2 << line[1]
-      @position3 << line[2]
-      @position4 << line[3]
-      @position5 << line[4]
-      @position6 << line[5]
-      @position7 << line[6]
-      @position8 << line[7]
-      @position9 << line[8]
-      @position10 << line[9]
-      @position11 << line[10]
-      @position12 << line[11]
+  def determine_power_consumption
+    power_bits = []
+
+    input.each do |line|
+      line.chars.each_with_index do |bit, index|
+        if power_bits[index].nil?
+          power_bits[index] = bit == '1' ? 1 : 0
+        elsif bit == '1'
+          power_bits[index] += 1
+        end
+      end
     end
+    gamma_rate = ''
+    epsilon_rate = ''
 
-    @position1.join(",").to_i
-    @position2.join(",").to_i
-    @position3.join(",").to_i
-    @position4.join(",").to_i
-    @position5.join(",").to_i
-    @position6.join(",").to_i
-    @position7.join(",").to_i
-    @position8.join(",").to_i
-    @position9.join(",").to_i
-    @position10.join(",").to_i
-    @position11.join(",").to_i
-    @position12.join(",").to_i
-
+    power_bits.each do |bit_position|
+      gamma_rate += bit_position > (input.size / 2) ? '1' : '0'
+      epsilon_rate += bit_position > (input.size / 2) ? '0' : '1'
+    end
+    gamma_rate.to_i(2) * epsilon_rate.to_i(2)
   end
-
-  def determine_gamma_rate
-    gamma_rate= []
-
-    gamma_rate << @position1.max_by {|pos| @position1.count(pos) }
-    gamma_rate << @position2.max_by {|pos| @position2.count(pos) }
-    gamma_rate << @position3.max_by {|pos| @position3.count(pos) }
-    gamma_rate << @position4.max_by {|pos| @position4.count(pos) }
-    gamma_rate << @position5.max_by {|pos| @position5.count(pos) }
-    gamma_rate << @position6.max_by {|pos| @position6.count(pos) }
-    gamma_rate << @position7.max_by {|pos| @position7.count(pos) }
-    gamma_rate << @position8.max_by {|pos| @position8.count(pos) }
-    gamma_rate << @position9.max_by {|pos| @position9.count(pos) }
-    gamma_rate << @position10.max_by {|pos| @position10.count(pos) }
-    gamma_rate << @position11.max_by {|pos| @position11.count(pos) }
-    gamma_rate << @position12.max_by {|pos| @position12.count(pos) }
-
-
-    gamma_rate.join("").to_i(2)
-  end
-
-  def determine_epsilon_rate
-    epsilon_rate= []
-    epsilon_rate << @position1.min_by {|pos| @position1.count(pos) }
-    epsilon_rate << @position2.min_by {|pos| @position2.count(pos) }
-    epsilon_rate << @position3.min_by {|pos| @position3.count(pos) }
-    epsilon_rate << @position4.min_by {|pos| @position4.count(pos) }
-    epsilon_rate << @position5.min_by {|pos| @position5.count(pos) }
-    epsilon_rate << @position6.min_by {|pos| @position6.count(pos) }
-    epsilon_rate << @position7.min_by {|pos| @position7.count(pos) }
-    epsilon_rate << @position8.min_by {|pos| @position8.count(pos) }
-    epsilon_rate << @position9.min_by {|pos| @position9.count(pos) }
-    epsilon_rate << @position10.min_by {|pos| @position10.count(pos) }
-    epsilon_rate << @position11.min_by {|pos| @position11.count(pos) }
-    epsilon_rate << @position12.min_by {|pos| @position12.count(pos) }
-    epsilon_rate.join("").to_i(2)
-  end
-
 end
 
 
 p = Part1.new
-puts p.determine_gamma_rate
-puts p.determine_epsilon_rate
-puts p.determine_gamma_rate * p.determine_epsilon_rate
+puts p.determine_power_consumption
--- 03/part2.rb
+++ 03/part2.rb
@@ -1,5 +1,84 @@
+require 'pry'
 class Part2
+  attr_reader :input
+
+  def initialize
+    @input = File.readlines("input2.txt", chomp: true)
+  end
+
+  def determine_bit_array(numbers)
+    power_bits = []
+
+    numbers.each do |line|
+      line.chars.each_with_index do |bit, index|
+        if power_bits[index].nil?
+          power_bits[index] = bit == '1' ? 1 : 0
+        elsif bit == '1'
+          power_bits[index] += 1
+        end
+      end
+    end
+    power_bits
+  end
+
+  def determine_ogygen_generator_rating
+    numbers = input.dup
+    index = 0
+
+    while numbers.length > 1
+      power_bits = determine_bit_array(numbers)
+      numbers = determine_ogygen_generator_rating_number(numbers, power_bits, index)
+      index += 1
+    end
+    #we have to continue this now for all other positions
+    numbers.first
+  end
+
+  def determine_ogygen_generator_rating_number(numbers, power_bits, index)
+     # determine the most common number in position index
+     number_of_ones = power_bits[index]
+     number_of_zeroes = numbers.size - number_of_ones
+
+     if number_of_ones >= number_of_zeroes
+      numbers.select! { |number| number[index] == '1' }
+     else
+      numbers.select! { |number| number[index] == '0' }
+     end
+    numbers
+  end
+
+  def determine_co2_scrubber_rating
+    numbers = input.dup
+    index = 0
+
+    while numbers.length > 1
+      power_bits = determine_bit_array(numbers)
+      numbers = determine_co2_generator_rating_number(numbers, power_bits, index)
+      index += 1
+    end
+    #we have to continue this now for all other positions
+    numbers.first
+  end
+
+  def determine_co2_generator_rating_number(numbers, power_bits, index)
+    # determine the most common number in position index
+    number_of_ones = power_bits[index]
+    number_of_zeroes = numbers.size - number_of_ones
+
+    if number_of_zeroes <= number_of_ones
+     numbers.select! { |number| number[index] == '0' }
+    else
+     numbers.select! { |number| number[index] == '1' }
+    end
+   numbers
+ end
 
 
 
 end
+
+
+p = Part2.new
+ogygen_generator_rating = p.determine_ogygen_generator_rating
+co2_scrubber_rating = p.determine_co2_scrubber_rating
+puts ogygen_generator_rating.to_i(2) * co2_scrubber_rating.to_i(2)
